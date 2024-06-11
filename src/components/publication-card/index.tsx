e014b160b2d90341523e916d635aeaef2d393cb2
Pranjal Naman <110885323+pranjalnam@users.noreply.github.com>
2024-06-11 21:29:50 +0530

    Update index.tsx

--- src/components/publication-card/index.tsx
+++ src/components/publication-card/index.tsx
@@ -87,7 +87,7 @@ const PublicationCard = ({
             <div className="w-full">
               <div className="px-4">
                 <div className="text-center w-full">
-                  <h2 className="text-xl-content opacity-60 mb-2">{item.title}</h2>
+                  <h2 className="font-sans font-medium opacity-90 mb-2">{item.title}</h2>
                   {item.conferenceName && (
                     <p className="text-base-content opacity-50 text-sm">
                       {item.conferenceName}
