288714edddee2ff0aec308f32bdedd681a263bc2
Pranjal Naman <110885323+pranjalnam@users.noreply.github.com>
2024-07-04 01:14:17 +0530

    added google tag

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -225,7 +225,7 @@ const CONFIG = {
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-NQRESM4LQW', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
   hotjar: {
